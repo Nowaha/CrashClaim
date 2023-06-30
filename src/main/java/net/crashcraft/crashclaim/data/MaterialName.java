7f390fd323bd0329bbd3a9b7e00028c3e2f5a327
Noah <noahverkaik@gmail.com>
2023-06-30 15:03:29 +0200

    Own implementation of material name instead of using a dependency for it

--- src/main/java/net/crashcraft/crashclaim/data/MaterialName.java
+++ src/main/java/net/crashcraft/crashclaim/data/MaterialName.java
@@ -1,26 +1,22 @@
 package net.crashcraft.crashclaim.data;
 
-import com.google.common.base.CaseFormat;
-import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Material;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class MaterialName {
     private final HashMap<Material, String> names;
 
-    public MaterialName(){
+    public MaterialName() {
         names = new HashMap<>();
 
-        for (Material material : Material.values()){
-            names.put(material,
-                    StringUtils.capitalize(StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(
-                            CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, material.name())
-                    ), ' ')));
+        for (Material material : Material.values()) {
+            names.put(material, String.join(" ", Arrays.stream(material.name().toLowerCase().split("_")).map(it -> it.toUpperCase().charAt(0) + it.toLowerCase().substring(1)).toArray(String[]::new)));
         }
     }
 
-    public String getMaterialName(Material material){
+    public String getMaterialName(Material material) {
         return names.get(material);
     }
 }
